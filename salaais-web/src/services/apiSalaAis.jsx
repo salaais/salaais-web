28a54b1194d7b93a0e7746d0317f38b1c84b8475
febreno <felipesugisawa1@gmail.com>
2025-03-19 10:26:22 -0300

    ajuste na url paymentPlan

--- salaais-web/src/services/apiSalaAis.jsx
+++ salaais-web/src/services/apiSalaAis.jsx
@@ -7,21 +7,15 @@ export const apiSalaAis = axios.create({
   },
 });
 
-export const paymentPlan = async (accessToken, productKey) => {
-  try {
-    const { data } = await apiSalaAis.get(
-      `${process.env.REACT_APP_SALA_AIS_API}stripe/pagamento-web${productKey}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      },
-    );
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Erro ao criar pagamento");
-  }
+export default apiSalaAis;
+
+export const paymentPlan = (accessToken, productKey) => {
+  return axios.get(`${process.env.REACT_APP_SALA_AIS_API}stripe/pagamento-web/${productKey}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
 };
 
-export default apiSalaAis;
+
